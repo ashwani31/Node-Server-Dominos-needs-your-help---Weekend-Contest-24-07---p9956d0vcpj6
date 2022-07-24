5eb4fc928d503ee442ecc54a1bd80f94a15a5329
ashwani31 <99326851+ashwani31@users.noreply.github.com>
2022-07-24 22:17:13 +0530

    1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 var http = require("http");
 
-const httpServer = http.createServer(handleServer);
-
-
-function handleServer(req, res) {
-  
-}
-
-module.exports = httpServer;
+var server = http.createServer(function (req, res) {
+  if (req.url == "/welcome") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html><body><p>Welcome to Dominos!</p><body><html>");
+    res.end();
+  } else if (req.url == "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(
+      "<html><body><p>phone no. 18602100000 and email.guestcaredominos@jublfood.com </p><body><html>"
+    );
+    res.end();
+  } else res.end("Invalid request 404");
+});
+server.listen(8081);
+console.log("server is running");
